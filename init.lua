8cf3f3a81c056e7d86a62cba23e936553a09e9f0
Safdar Ali <119604831+SafdarAlli@users.noreply.github.com>
2025-05-28 11:20:08 +0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -167,7 +167,7 @@ vim.opt.confirm = true
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-
+vim.keymap.set('i', 'jk', '<Esc>', { noremap = true, silent = true })
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
